a243035dd132de6176935bf5eeb71a6e4ce95c01
souadhimel <souad.anam@gmail.com>
2022-04-03 16:03:07 +0600

    foot

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-
-
-import { Route, Routes } from 'react-router-dom';
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import Coins from './components/Coins/Coins';
-import Contact from './components/Contact/Contact';
-import About from './components/About/About';
+import React  from 'react';
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Coins from "./components/Coins/Coins";
+import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
 import Footer from './components/Footer/Footer';
+
 
 function App() {
   return (
     <div>
+      <h1>hello</h1>
       <Header></Header>
 
       <Routes>
-<Route path="/" element={<Home></Home>}></Route>
-<Route path="/coins" element={<Coins></Coins>}></Route>
-<Route path="/contact" element={<Contact></Contact>}></Route>
-<Route path="/about" element={<About></About>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/coins" element={<Coins></Coins>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
       </Routes>
       <Footer></Footer>
-      
+  
     </div>
   );
 }
---
+++ src/components/Contact/UScontact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UScontact = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UScontact;
